846239d57a9389601fbe8a22229239ed40500434
Kian Harris <kianharris@Kians-MacBook-Air.local>
2024-10-30 15:33:41 +0100

    working with pandas

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 import streamlit as st
+import pandas as pd
+from frequency_analyser import frequency_analyser
 
-st.title("A New Website!!!")
-st.header("We're brainstorming this")
-st.subheader("Don't even get me started on this")
-st.markdown("""
-PLEASE **NO**
-REALLY PLEASE NO
-SAVE ME, PLEASE HELP
-            """)
 
-values = st.slider("Select a range of values", 0.0, 100.0, (25.0, 75.0))
-st.write(f"Values: {values}")
+st.title("Frequency Analyser")
+txt = st.text_area(
+    "Text to analyze",
+    "It was the best of times, it was the worst of times, it was the age of "
+    "wisdom, it was the age of foolishness, it was the epoch of belief, it "
+    "was the epoch of incredulity, it was the season of Light, it was the "
+    "season of Darkness, it was the spring of hope, it was the winter of "
+    "despair, (...)",
+)
 
-st.balloons()
+result = frequency_analyser(txt)
+df = pd.DataFrame(result).T
+df.columns = ["Letter", "Count", "%"]
+
+st.dataframe(
+    df,
+    hide_index=True,
+    height = 900
+)
---
+++ frequency_analyser.py
@@ -0,0 +1,13 @@
+#Frequency Analyser
+
+def frequency_analyser(text: str):
+    alphabet = [['a','b','c','d','e','f','g','h','i','k','j','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
+                [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+                [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0]]         #Create a 2d of the alphabet and the frequency
+
+    for i in range(0, len(alphabet[0])):            #Iterate over each character in the alphabet
+
+        alphabet[1][i] = text.count(alphabet[0][i])            #Get the total character count for the character being checked
+        alphabet[2][i] = text.count(alphabet[0][i])/len(text)*100         #Get the percentage frequency that it occurs
+
+    return alphabet          #Returns the results
