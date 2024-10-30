e82b35749bd31bc54d31afb0ea1b27a03a1f69c9
Kian Harris <kianharris@Kians-MacBook-Air.local>
2024-10-30 19:28:11 +0100

    Got multipage working

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ home_page = st.Page("pages/1_Home.py", title="Home")
 frequency_analyser_page = st.Page("pages/2_Frequency_Analyser.py", title="Frequency Analyser")
 
 navigation_bar = st.navigation({
-     "Tools": [home_page, st.Page("pages/2_Frequency_Analyser.py", title="Frequency Analyser")]
+    "Home": [home_page],
+     "Tools": [frequency_analyser_page]
 })
 
 navigation_bar.run()
